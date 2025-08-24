10b589d99681b0d35bea532ee313cb30099bf35a
Topskys <2477350343@qq.com>
2025-08-24 15:44:15 +0800

    ✨ feat(css): 配置tailwindcss及路径别名

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,18 +1,32 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
   },
   preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: 'src/renderer/assets/**',
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@assets': resolve('src/renderer/assets'),
+        '@hooks': resolve('src/renderer/src/hooks'),
+        '@store': resolve('src/renderer/src/store'),
+        '@components': resolve('src/renderer/src/components'),
+        '@mocks': resolve('src/renderer/src/mocks')
       }
     },
     plugins: [react()]
